8d2acfe5a11cec06fc52bbd77c3d73560ae6ca81
Andrew Woo <andrew.woo@verizon.net>
2018-11-29 16:52:58 -0500

    Updates

--- precache-manifest.9e2a980c7f4d2bab15595eab9d69cb3b.js
+++ precache-manifest.9e2a980c7f4d2bab15595eab9d69cb3b.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/chameleon-gh/static/js/runtime~main.86d52806.js"
   },
   {
-    "revision": "26e837cefbbf3565fd8a",
-    "url": "/chameleon-gh/static/js/main.26e837ce.chunk.js"
+    "revision": "90578f1621dc7f24d26c",
+    "url": "/chameleon-gh/static/js/main.90578f16.chunk.js"
   },
   {
     "revision": "a57b25938a2e9850d270",
     "url": "/chameleon-gh/static/js/1.a57b2593.chunk.js"
   },
   {
-    "revision": "26e837cefbbf3565fd8a",
-    "url": "/chameleon-gh/static/css/main.7cbdd0e2.chunk.css"
+    "revision": "90578f1621dc7f24d26c",
+    "url": "/chameleon-gh/static/css/main.a2879143.chunk.css"
   },
   {
-    "revision": "18967933e4c52d7d98ecf377d2952881",
+    "revision": "f915780dd7e32b0208c4016dd8bb0641",
     "url": "/chameleon-gh/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/chameleon-gh/precache-manifest.141996116e7981cfa137302acb24e785.js"
+  "/chameleon-gh/precache-manifest.9e2a980c7f4d2bab15595eab9d69cb3b.js"
 );
 
 workbox.clientsClaim();
